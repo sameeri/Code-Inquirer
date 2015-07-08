865410384dad578e23f1188bdc4ebeda5bda6a6a
Sameeri Marryboyina <smarryboyina@telogical.com>
2015-07-08 06:29:26 -0700

    number based questions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var Joi = require('joi');
+var _ = require('lodash');
 
 function processAnswers(answers){
   console.log("And your answers are:", answers);
@@ -9,7 +10,13 @@ function validateName(name){
         return name !== '';
     }
 
-function validateLName(name){
+function validateAge(age)
+{
+   var reg = /^\d+$/;
+   return reg.test(age) || "Age should be a number!";
+}
+
+function validateLastName(name){
        var result =  true;
            Joi.validate(name, Joi.string().required(), function(err,val){
            if (err){
@@ -28,12 +35,19 @@ var questions = [
     type: "input",
     name: "firstName",
     validate: validateName
-},{
+},
+{
     message: "What's your last name?",
     type: "input",
     name: "lastName",
-    validate: validateLName
-},
+    validate: validateName
+},  
+{
+    message: "What's your age?",
+    type: "input",
+    name: "age",
+    validate: validateAge
+},    
 
 ];
 inquirer.prompt(questions, processAnswers);
